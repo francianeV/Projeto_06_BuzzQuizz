8183ee0659f54ca3ec94f300b83d14a18f72b0bd
Franciane <franciane.penina@gmail.com>
2022-04-20 17:39:50 -0300

    Botao de criar quizz finalizado

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function criarQuizz(){
+    const criar = document.querySelector(".conteudo");
+    criar.classList.add("oculto");
+
+    const telaCriar = document.querySelector(".montar-quizz");
+    telaCriar.classList.remove("oculto");
+}
+
