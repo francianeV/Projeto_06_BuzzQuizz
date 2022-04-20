6a8c42511681ce1b23afc71e740e8ac47fc38d23
Franciane <franciane.penina@gmail.com>
2022-04-20 19:21:10 -0300

    listagem de todos os quizzes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+let listaQuizes = [];
+
+listarQuiz()
+
+function listarQuiz() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promessa.then(carregarQuiz);
+}
+
+function carregarQuiz(response) {
+    listaQuizes = response.data
+    renderizarQuiz()
+}
+
+function renderizarQuiz () {
+    const ulQuiz = document.querySelector(".quizzes"); //alterar ".quizes" se necessário. ".quizes" é classe da tag <ul>
+    ulQuiz.innerHTML = ""
+    //criar function acessarQuiz() em: img -> onclick="acessarQuiz()"
+    for (let i=0; i<listaQuizes.length;i++) {
+        ulQuiz.innerHTML += `
+        <li class="quizz">
+            <img onclick="acessarQuiz()" src="${listaQuizes[i].image}"> 
+            <span>${listaQuizes[i].title}</sapn>
+        </li>
+        `;
+    }
+}
+
+function acessarQuiz() {
+    //acessa quiz abrindo a tela 2
+}
+
 function criarQuizz(){
     const criar = document.querySelector(".conteudo");
     criar.classList.add("oculto");
@@ -5,4 +37,3 @@ function criarQuizz(){
     const telaCriar = document.querySelector(".montar-quizz");
     telaCriar.classList.remove("oculto");
 }
-
