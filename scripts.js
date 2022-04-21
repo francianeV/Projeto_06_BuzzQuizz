7b0c0c033fd863f97039351bcf49075bab32a792
Franciane <franciane.penina@gmail.com>
2022-04-21 01:46:22 -0300

    topo do quiz da tela 2

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let listaQuizes = [];
 listarQuiz()
 
 function listarQuiz() {
-    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"); /*mudar para v6 depois*/
     promessa.then(carregarQuiz);
 }
 
@@ -18,16 +18,54 @@ function renderizarQuiz () {
     //criar function acessarQuiz() em: img -> onclick="acessarQuiz()"
     for (let i=0; i<listaQuizes.length;i++) {
         ulQuiz.innerHTML += `
-        <li class="quizz">
-            <img onclick="acessarQuiz()" src="${listaQuizes[i].image}"> 
-            <span>${listaQuizes[i].title}</sapn>
+        <li class="quizz" onclick="acessarQuiz(this)">
+            <img src="${listaQuizes[i].image}"> 
+            <span>${listaQuizes[i].title}</span>
+            <div class="quizz-id">${listaQuizes[i].id}</div>
         </li>
         `;
     }
 }
 
-function acessarQuiz() {
-    //acessa quiz abrindo a tela 2
+function acessarQuiz(element) {
+
+    let idQuizz = element.querySelector(".quizz-id").innerHTML;
+    console.log(idQuizz);
+  
+    const ocultar = document.querySelector(".conteudo");
+    ocultar.classList.add("oculto");
+
+    const irQuiz = document.querySelector(".pagina-quiz");
+    irQuiz.classList.remove("oculto");
+
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`);
+    
+    promise.then(chamarQuiz);
+
+
+
+
+}
+/*
+document.querySelectorAll(".quizz img").forEach( function(button) {
+    
+    button.addEventListener("click", function(event) {
+    const el = event.target || event.srcElement;
+    const numId = el.id;
+    console.log(numId);
+
+});
+});
+*/
+function chamarQuiz(response){
+    let idClicado = Number()
+
+    document.querySelector(".pagina-quiz").innerHTML+=` 
+    <header class="topo-quiz">
+    <img src="${response.data.image}" alt="">
+    <p>${response.data.title}</p>
+    </header>`;
+
 }
 
 function criarQuizz(){
@@ -37,3 +75,6 @@ function criarQuizz(){
     const telaCriar = document.querySelector(".montar-quizz");
     telaCriar.classList.remove("oculto");
 }
+
+
+
